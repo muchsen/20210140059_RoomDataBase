f1657bf1e352da83652c9e963eb456cf7e08694e
TA1059 <muchsen.ft21@mail.umy.ac.id>
2023-12-06 12:08:15 +0700

    class UIStateSiswa

--- app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
+++ app/src/main/java/com/example/roomsiswa/model/EntryViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.roomsiswa.model
-
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
 data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
